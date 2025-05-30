2bf2ad6964d7888ff87caf05cf3c68cd2131efa5
Melanie <melanibarroso13@gmail.com>
2025-05-30 12:26:23 +0200

    cambios

--- TrabajoFinalDeGrado/TrabajoFinalDeGrado/settings.py
+++ TrabajoFinalDeGrado/TrabajoFinalDeGrado/settings.py
@@ -165,10 +165,11 @@ DATABASES = {
 
 ALLOWED_HOSTS = ['tfg-l9oc.onrender.com', 'localhost', '127.0.0.1']
 
-import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'escuela_teatro/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
